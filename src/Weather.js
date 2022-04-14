cc98ca5238c8b0480c458be1cc481b9ce3fe8051
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-04-14 11:14:20 +0300

    added weather component to cater for UI

---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+export default function Weather(){
+    <div className="Weather">
+        <h1>Weather Search Engine</h1>
+      <form>
+        <input type="search" placeholder="Type city"/>
+        <input type="submit" value="search"/>
+        </form>
+    </div>
+}
